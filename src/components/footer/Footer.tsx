6ffa770e3e4706f4f60f601d839de408b0ef6801
andreipogi2727 <ahaboc3536@conestogac.on.ca>
2024-03-25 16:58:25 -0400

    Created the footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,10 @@ import "./footer.scss"
 
 const Footer = () => {
   return (
-    <div className ="footer">footer</div>
+    <div className ="footer">
+      <span>admin</span>
+      <span>@Andrei Dev Admin Dashboard</span>
+    </div>
   )
 }
 
