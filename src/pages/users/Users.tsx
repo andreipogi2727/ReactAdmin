6a09e9edd365f2ecc768a2730dce5e3fb0ed4fb5
andreipogi2727 <ahaboc3536@conestogac.on.ca>
2024-03-26 00:54:23 -0400

    Created the users data grid with own data

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,5 +1,53 @@
+import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable"
 import "./users.scss"
+import { userRows } from "../../data";
+
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+        field: "avatar", headerName:"Avatar",width:100,
+        renderCell: (params) =>{
+            return <img src ={params.row.img || "/noavatar.png"} alt="" />
+        },
+    },
+    {
+      field: 'firstName',
+      headerName: 'First name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last name',
+      width: 150,
+      editable: true,
+    },
+   
+    {
+        field: 'email',
+        headerName: 'Email',
+        width: 150,
+        editable: true,
+    },
+    {
+        field: 'phone',
+        headerName: 'Phone Number',
+        width: 150,
+        editable: true,
+    },
+    {
+        field: 'createdAt',
+        headerName: 'Created At',
+        width: 150,
+        editable: true,
+    },
+    {
+        field: "verified", headerName: "Verified", width: 100, type: "boolean"
+
+    },
+
+  ];
 
 const Users = () => {
     return (
@@ -8,7 +56,7 @@ const Users = () => {
                 <h1>Users</h1>
                 <button>Add New User</button>
             </div>
-            <DataTable/>
+            <DataTable columns = {columns} rows = {userRows}/>
 
         </div>
     )
