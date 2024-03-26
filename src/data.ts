44ed81fed2eca035048b19da5067b3d35d15a917
andreipogi2727 <ahaboc3536@conestogac.on.ca>
2024-03-25 20:18:56 -0400

    Populated the other three boxes with own data

--- src/data.ts
+++ src/data.ts
@@ -167,4 +167,58 @@ export const chartBoxUser = {
         {name: "Sat", users: 450},
     ],
 };
+export const chartBoxProduct = {
+    color: "skyblue",
+    icon: "/productIcon.svg",
+    title: "Total Products",
+    number: "238",
+    dataKey: "products",
+    percentage: 21,
+    chartData: 
+    [
+        {name: "Sun", products: 400},
+        {name: "Mon", products: 600},
+        {name: "Tues", products: 500},
+        {name: "Wed", products: 700},
+        {name: "Thu", products: 400},
+        {name: "Fri", products: 500},
+        {name: "Sat", products: 450},
+    ],
+};
+export const chartBoxRevenue = {
+    color: "teal",
+    icon: "/revenueIcon.svg",
+    title: "Total Revenue",
+    number: "$56.432",
+    dataKey: "revenue",
+    percentage: -12,
+    chartData: 
+    [
+        {name: "Sun", revenue: 400},
+        {name: "Mon", revenue: 600},
+        {name: "Tues", revenue: 500},
+        {name: "Wed", revenue: 700},
+        {name: "Thu", revenue: 400},
+        {name: "Fri", revenue: 500},
+        {name: "Sat", revenue: 450},
+    ],
+};
+export const chartBoxConversion = {
+    color: "gold",
+    icon: "/conversionIcon.svg",
+    title: "Total Ratio",
+    number: "2.6",
+    dataKey: "ratio",
+    percentage: 12,
+    chartData: 
+    [
+        {name: "Sun", ratio: 400},
+        {name: "Mon", ratio: 600},
+        {name: "Tues", ratio: 500},
+        {name: "Wed", ratio: 700},
+        {name: "Thu", ratio: 400},
+        {name: "Fri", ratio: 500},
+        {name: "Sat", ratio: 450},
+    ],
+};
 
