c5533629a30795619115feff8222aac5fa745e32
andreipogi2727 <ahaboc3536@conestogac.on.ca>
2024-03-26 01:23:46 -0400

    Made the user grid dynamic

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,19 +1,42 @@
 import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid"
 import "./dataTable.scss"
+import { Link } from "react-router-dom"
 
 
 
 type Props = {
     columns: GridColDef[],
-    rows: object[]
+    rows: object[],
+    slug: string;
+};
+
+const handleDelete = (id:number)=>{
+    console.log(id + " has been deleted")
 }
 const DataTable = (props:Props) => {
+    const actionColumn:GridColDef = {
+        field:"ation",
+        headerName: "Action",
+        width:200,
+        renderCell:(params)=>{
+            return(
+                <div className="action">
+                    <Link to={`/${props.slug}/${params.row.id}}/`}>
+                    <img src="/view.svg" alt="" />
+                    </Link>
+                    <div className="delete" onClick={()=>handleDelete(params.row.id)}>
+                        <img src="/delete.svg" alt="" />
+                    </div>
+                </div>
+            )
+        }
+    }
   return (
     <div className="dataTable">
          <DataGrid
          className="dataGrid"
         rows={props.rows}
-        columns={props.columns}
+        columns={[...props.columns, actionColumn]}
         initialState={{
           pagination: {
             paginationModel: {
